bdf71a464b3bc7625e0f4300fd74cd4b115589ba
SATHISH1-234 <139840633+SATHISH1-234@users.noreply.github.com>
2024-07-16 19:53:29 +0530

    Time: 16 ms (8.59%), Space: 9 MB (6.57%) - LeetHub

---
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.c
@@ -0,0 +1,22 @@
+int mostWordsFound(char** sentences, int sentencesSize) {
+    int max=0;
+    for(int i=0;i<sentencesSize;i++)
+    {
+        char word[400][400];
+        int m=0,n=0;
+        for(int j=0;j<strlen(sentences[i]);j++)
+        {
+            if(isspace(sentences[i][j]))
+            {
+                word[m][n]='\0';
+                m++;
+                n=0;
+                continue;
+            }
+            word[m][n++]=sentences[i][j];
+        }
+        if(m>max) max=m;
+        else continue;
+    }
+    return max+1;
+}
