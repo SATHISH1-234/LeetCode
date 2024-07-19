47e9b719a6672693ada8627fd4c4894e432cbb22
SATHISH1-234 <139840633+SATHISH1-234@users.noreply.github.com>
2024-07-19 11:36:08 +0530

    Time: 1137 ms (5.02%), Space: 9.5 MB (5.76%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,21 +1,14 @@
 int singleNumber(int* nums, int numsSize) {
-    
-    int res;
-    
     if(numsSize<=1)
         return nums[0];
     for(int i=0;i<numsSize-1;i++)
     {
-            for(int j=i+1;j<numsSize;j++){
-                if(nums[i]==nums[j]) {
-                    nums[i]=0;
-                    nums[j]=0;
-                } }
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]==nums[j])
+             {      nums[i]=0;
+                    nums[j]=0;} }
     }
     for(int i=0;i<numsSize;i++)
-    {
         if(nums[i]!=0) return nums[i];
-    }
-  
-    return res;
+    return 0;
 }
