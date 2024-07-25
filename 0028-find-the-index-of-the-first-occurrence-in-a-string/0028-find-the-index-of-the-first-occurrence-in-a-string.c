a5b0b6b47f0627f2932943eff68f4e5b8eb3a830
SATHISH1-234 <139840633+SATHISH1-234@users.noreply.github.com>
2024-07-25 12:08:25 +0530

    Time: 4 ms (36.43%), Space: 8 MB (7.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -2,17 +2,11 @@ int strStr(char* s1, char* s2) {
     int len1 = strlen(s1);
     int len2 = strlen(s2);
     int i, j;
-
-    // If s2 is longer than s1, return -1
     if (len1 < len2) return -1;
-
-    // If s1 and s2 have the same length, check if they are equal
     if (len1 == len2) {
         if (strcmp(s1, s2) == 0) return 0;
         else return -1;
     }
-
-    // Iterate through s1 to find s2
     for (i = 0; i <= len1 - len2; i++) {
         int flag = 0;
         for (j = 0; j < len2; j++) {
@@ -21,9 +15,9 @@ int strStr(char* s1, char* s2) {
                 break;
             }
         }
-        if (flag == 0) return i; // Found s2 at position i
+        if (flag == 0) return i;
     }
 
-    return -1; // s2 not found in s1
+    return -1;
 }
     
