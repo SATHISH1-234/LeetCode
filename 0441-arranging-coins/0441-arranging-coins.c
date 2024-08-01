e73bbbdf1cb64c9d0cf28993c39dfa089b181f09
SATHISH1-234 <139840633+SATHISH1-234@users.noreply.github.com>
2024-08-01 12:45:27 +0530

    Time: 0 ms (100%), Space: 8.5 MB (21.94%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.c
+++ 0441-arranging-coins/0441-arranging-coins.c
@@ -1,14 +1,34 @@
 int arrangeCoins(int n) {
-    int count=0;
-    if(n==1 || n==0) return n;
-    for(int i=1;i<n+1;i++)
-    {
-        if(i<=n) {
-            count++;
-            if(n!=0){
-            n-=i;}
+    // int count=0;
+    // if(n==1 || n==0) return n;
+    // for(int i=1;i<n+1;i++)
+    // {
+    //     if(i<=n) {
+    //         count++;
+    //         if(n!=0){
+    //         n-=i;}
+    //     }
+    //     else break;
+    // }
+    // return count;
+
+    long left = 0, right = n;
+    long k, curr;
+
+    while (left <= right) {
+        k = left + (right - left) / 2;
+        curr = k * (k + 1) / 2;
+
+        if (curr == n) {
+            return (int)k;
+        } else if (curr < n) {
+            left = k + 1;
+        } else {
+            right = k - 1;
         }
-        else break;
     }
-    return count;
+
+    return (int)right;
+
+
 }
