d3d4255152ce766b35c198ca827ae1f75c5ed4bf
SATHISH1-234 <139840633+SATHISH1-234@users.noreply.github.com>
2024-09-13 10:46:03 +0530

    Time: 0 ms (100%), Space: 7.4 MB (94.66%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.c
@@ -0,0 +1,26 @@
+bool isprime(int n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3 || n == 5) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    
+   
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool isUgly(int n) {
+    if (n <= 0) return false; 
+    
+   
+    while (n % 2 == 0) n /= 2;
+    while (n % 3 == 0) n /= 3;
+    while (n % 5 == 0) n /= 5;
+    
+   
+    return n==1;
+}
