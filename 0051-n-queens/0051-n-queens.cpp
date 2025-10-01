0287940f591041024262cbdb7c1f1e78f2113fb5
SATHISH1-234 <139840633+SATHISH1-234@users.noreply.github.com>
2025-10-01 09:56:41 +0530

    Time: 62 ms (5.66%), Space: 29.3 MB (8.95%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        backtrack(0, n, board, {}, {}, {});
+        return res;
+    }
+
+    void backtrack(int row, int n, vector<string>& board,
+                   unordered_set<int> cols,
+                   unordered_set<int> diag1,
+                   unordered_set<int> diag2) {
+        if (row == n) {
+            res.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (cols.count(col) || diag1.count(row - col) || diag2.count(row + col))
+                continue;
+
+            // Place queen
+            board[row][col] = 'Q';
+            cols.insert(col);
+            diag1.insert(row - col);
+            diag2.insert(row + col);
+
+            backtrack(row + 1, n, board, cols, diag1, diag2);
+
+            // Backtrack
+            board[row][col] = '.';
+            cols.erase(col);
+            diag1.erase(row - col);
+            diag2.erase(row + col);
+        }
+    }
+};
